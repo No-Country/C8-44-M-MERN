42d0f621f5b275fbc54d832893e5b6eeee4e1b2a
nathaliariascos <riascosnathalia6@gmail.com>
2022-11-14 11:22:54 -0500

    configuraciones iniciales

---
+++ front/tailwind.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Quicksand': ['"Quicksand"', 'sans-serif'],
+      },
+    },
+    colors: {
+      'pink-dark': '#e26868',
+      'pink-light': '#ff8787',
+      'gray-dark': '#35424a',
+      'gray': '#8492a6',
+      'gray-light': '#ededed',
+      'white': '#ffff'
+    },
+  },
+  plugins: [],
+}

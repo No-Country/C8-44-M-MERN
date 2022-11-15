18039da794e5bf34c16e5b54a15cffe31266e917
Herrera <herrera.cesar.arg@outlook.com>
2022-11-15 16:36:20 -0300

    Agregando estilos a formularios

--- front/src/pages/Login/Login.tsx
+++ front/src/pages/Login/Login.tsx
@@ -1,14 +1,25 @@
-import { LoginForm } from './components'
+import { LoginForm } from "./components";
 
-type Props = {}
+type Props = {};
 
 const Login = (props: Props) => {
   return (
-    <div className='main-container'>
-      <h1 className='title'>Sign In</h1>
+    <div className="main-container flex flex-col justify-between sm:px-32 md:px-36 lg:px-64 xl:px-[550px] xl:pt-24 xl:pb-20">
+      <div className="flex flex-col gap-2">
+        <h1 className="title text-secondary-dark">Sign In</h1>
+        <p className="text-secondary-regular text-sm">
+          Hi there! Nice to see you again.
+        </p>
+      </div>
       <LoginForm />
+      <div className="flex flex-row gap-2 justify-center align-middle">
+        <p className="text-secondary-regular text-sm">Don't have an Account?</p>
+        <a href="" className="text-primary-dark text-sm">
+          Sign Up
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;

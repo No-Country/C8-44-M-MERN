697e3a2535e239620ecf7ca782180a346137fbb0
S3ck1 <seck.dv15@gmail.com>
2022-11-15 00:08:37 -0600

    Containers added to form sections

--- front/src/pages/Login/components/LoginForm.tsx
+++ front/src/pages/Login/components/LoginForm.tsx
@@ -8,39 +8,47 @@ type FormValues = {
 };
 
 const Form = (props: Props) => {
-  const { register, handleSubmit, formState: { errors } } = useForm<FormValues>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>();
   const onSubmit: SubmitHandler<FormValues> = (data) => console.log(data);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="auth-form">
-      <label htmlFor="email-input">Email</label>
-      <input
-        type="email"
-        id="email-input"
-        placeholder="Your email address"
-        {...register("email", {
-          required: true,
-          pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-        })}
-        className="border border-solid"
-      />
-      {errors?.email?.type === "required" && (
-        <p className="form-error"> This field is required</p>
-      )}
-      {errors?.email?.type === "pattern" && (
-        <p className="form-error"> Please insert a valid email</p>
-      )}
-      <label htmlFor="password-input">Password</label>
-      <input
-        type="password"
-        id="password-input"
-        placeholder="Your password"
-        {...register("password", { required: true })}
-        className="border border-solid"
-      />
-      {errors?.password?.type === "required" && (
-        <p className="form-error"> This field is required</p>
-      )}
+      <div className="form-group">
+        <label htmlFor="email-input">Email</label>
+        <input
+          type="email"
+          id="email-input"
+          placeholder="Your email address"
+          {...register("email", {
+            required: true,
+            pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+          })}
+          className="border border-solid"
+        />
+        {errors?.email?.type === "required" && (
+          <p className="form-error"> This field is required</p>
+        )}
+        {errors?.email?.type === "pattern" && (
+          <p className="form-error"> Please insert a valid email</p>
+        )}
+      </div>
+      <div className="form-group">
+        <label htmlFor="password-input">Password</label>
+        <input
+          type="password"
+          id="password-input"
+          placeholder="Your password"
+          {...register("password", { required: true })}
+          className="border border-solid"
+        />
+        {errors?.password?.type === "required" && (
+          <p className="form-error"> This field is required</p>
+        )}
+      </div>
       <button type="submit" className="border border-solid mt-4">
         Sign In
       </button>

0c8e4a3e9313971dc76fd71bd27195c4ae9c50a9
Herrera <herrera.cesar.arg@outlook.com>
2022-11-16 10:14:53 -0300

    Creacion de componentes Home y Navbar

---
+++ front/src/pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import { Navbar } from "../../components";
+
+const Home = () => {
+  return (
+    <>
+      <div className="main-container flex flex-col justify-between">
+        <h1 className="title text-secondary-dark">Home</h1>
+        <img src={""} alt="" />
+      </div>
+      <Navbar />
+    </>
+  );
+};
+
+export default Home;

9a4d93fc41bb0d469ba2d33f6e6ba0c3e1a30bab
nathaliariascos <riascosnathalia6@gmail.com>
2022-11-15 23:07:19 -0500

    se añadio estilos a la pagina de registro

--- front/src/pages/Register/Register.tsx
+++ front/src/pages/Register/Register.tsx
@@ -1,12 +1,22 @@
+import { Link } from 'react-router-dom'
 import { RegisterForm } from './components'
 
-type Props = {}
-
-const Register = (props: Props) => {
+const Register = () => {
   return (
-    <div className='main-container'>
-      <h1 className='title'>Sign Up</h1>
-      <RegisterForm />
+    <div className='main-container flex flex-col items-center'>
+      <div className='flex flex-col gap-9 sm:w-96'>
+        <h1 className='title'>Sign Up</h1>
+        <RegisterForm />
+        <p className='flex gap-2 text-secondary-regular justify-center'>
+          Have an Account?  
+          <span className='text-primary-dark font-semi'>
+            <Link to='/login'>
+              Sign In
+            </Link>
+          </span>   
+        </p>
+      </div>
+      
     </div>
   )
 }

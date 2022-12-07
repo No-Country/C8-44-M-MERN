f5e7fd619593bdc67f8ded3160072be52ff96221
S3ck1 <seck.dv15@gmail.com>
2022-12-06 22:51:00 -0600

    add habit responsive done

--- front/src/pages/AddHabit/AddHabit.tsx
+++ front/src/pages/AddHabit/AddHabit.tsx
@@ -24,13 +24,13 @@ const AddHabit = () => {
   const addHabitHandler = async () => {
     await dispatch(addHabit(selectedHabit));
     navigate("/home");
-  }
+  };
 
   return (
     <>
-      <div className="main-container flex flex-col gap-4">
+      <div className="main-container flex flex-col gap-8">
         <Header title="Add Habit" />
-        <ul>
+        <ul className="lg:grid lg:grid-cols-4 lg:gap-5 2xl:gap-0">
           {habitsList.habits.map((habit) => (
             <li key={habit?._id}>
               <div
@@ -38,27 +38,40 @@ const AddHabit = () => {
                   border: habit?._id === selectedHabit ? "1px solid" : "",
                 }}
                 onClick={() => selectedHabitHandler(habit?._id)}
-                className="items-center justify-between rounded-md bg-secondary-light/30 w-full my-3"
+                className="flex flex-col items-center justify-center rounded-md bg-secondary-light/30 w-full my-3 
+                lg:hover:bg-secondary-light lg:h-36 lg:w-36 xl:h-52 xl:w-52 2xl:h-72 2xl:w-72"
               >
-                <h3 className="text-sm text-center pl-5 p-3 text-secondary-dark dark:text-secondary-light">
+                <h3
+                  className="text-sm text-center pl-5 p-3 text-secondary-dark dark:text-secondary-light 
+                  lg:px-0 xl:px-5 lg:text-base xl:text-xl lg:text-primary-dark lg:font-normal 2xl:text-2xl"
+                >
                   {habit?.name}
                 </h3>
+                <h4 className="hidden lg:block lg:text-sm h-24 text-center text-secondary-regular lg:mx-2 2xl:text-xl 2xl:mx-5">
+                  {habit?.description}
+                </h4>
               </div>
             </li>
           ))}
         </ul>
-        <button
-          className="border border-solid bg-primary-dark text-secondary-light font-bold h-10 rounded-xl"
-          disabled={!selectedHabit}
-          onClick={addHabitHandler}
-        >
-          Add Habit
-        </button>
-        <Link to="/add-custom-habit">
-          <button className="border border-solid border-primary-dark text-primary-dark bg-white font-semibold h-10 rounded-xl">
-            Create new Habit
+        <div className="flex flex-col gap-2 lg:flex-row lg:justify-end xl:mt-8">
+          <button
+            className="border border-solid bg-primary-dark text-white font-bold h-10 rounded-xl 
+            lg:w-52 lg:h-12 xl:w-80 xl:h-16 2xl:w-96 2xl:h-20 2xl:text-2xl"
+            disabled={!selectedHabit}
+            onClick={addHabitHandler}
+          >
+            Add Habit
           </button>
-        </Link>
+          <Link
+            className="border border-solid border-primary-dark text-primary-dark bg-white font-semibold 
+            h-10 rounded-xl text-center flex justify-center items-center 
+            lg:w-52 lg:h-12 xl:w-80 xl:h-16 2xl:w-96 2xl:h-20 2xl:text-2xl"
+            to="/add-custom-habit"
+          >
+            <button>Create new Habit</button>
+          </Link>
+        </div>
       </div>
     </>
   );

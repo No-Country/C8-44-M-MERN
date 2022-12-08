dda797c14bcff3a02d4148cf07f7c5447830d74c
César <herrera.cesar.arg@outlook.com>
2022-12-07 21:31:20 -0300

    Fixing populate

--- back/src/controllers/habits.controllers.js
+++ back/src/controllers/habits.controllers.js
@@ -1,91 +1,82 @@
-const UsersDaoMongoDB = require("../DAOs/usersDaoMongoDb")
-const HabitsDaoMongoDB = require("../DAOs/habitsDaoMongoDb")
+const UsersDaoMongoDB = require('../DAOs/usersDaoMongoDb');
+const HabitsDaoMongoDB = require('../DAOs/habitsDaoMongoDb');
 const habitsApi = new HabitsDaoMongoDB();
 const usersApi = new UsersDaoMongoDB();
-
 
 const getAllHabits = async (req, res, next) => {
   try {
     const habits = await habitsApi.getAll();
-    console.log(habits)
-    res.json(habits)
+    console.log(habits);
+    res.json(habits);
   } catch (error) {
     next({
       status: 400,
       errorContent: error,
-      message: "Algo salio mal",
+      message: 'Algo salio mal',
     });
   }
 };
 
 const getHabitById = async (req, res, next) => {
   try {
-    const {id} = req.params;
-    const habit = await habitsApi.findOneById(id)
-    res.json(habit)
+    const { id } = req.params;
+    const habit = await habitsApi.findOneById(id);
+    res.json(habit);
   } catch (error) {
     next({
       status: 400,
       errorContent: error,
-      message: "Algo salio mal",
+      message: 'Algo salio mal',
     });
   }
 };
 
 const createHabitAdmin = async (req, res, next) => {
   try {
-    console.log("hi")
-    const { 
+    console.log('hi');
+    const { name, description, category } = req.body;
+    const newHabit = {
       name,
       description,
       category,
-    } = req.body;
-    const newHabit = {
-        name,
-        description,
-        category,
-        experience:0,
-        frecuency:"each day",
-        isDone:false
+      experience: 0,
+      frecuency: 'each day',
+      isDone: false,
     };
     console.log(newHabit);
     habitsApi.save(newHabit);
-    res.status(201).json(newHabit)
+    res.status(201).json(newHabit);
   } catch (error) {
     next({
       status: 400,
       errorContent: error,
-      message: "Algo salio mal",
+      message: 'Algo salio mal',
     });
   }
 };
 
 const createCustomHabit = async (req, res, next) => {
   try {
-    let user = await usersApi.findOneById(req.user.id);       //este user soy YO
-    const { 
-      name,
-      description,
-      category,
-    } = req.body;
+    let user = await usersApi.findOneById(req.user.id); //este user soy YO
+    const { name, description, category } = req.body;
     const newHabit = {
       name,
       description,
       category,
-      experience:0,
-      frecuency:"each day",
-      isDone:false
-  };
-    console.log(user)
-    console.log(user.habits,"hello")
-    user.habits.push(newHabit);                            //pusheo al key followers
+      experience: 0,
+      frecuency: 'each day',
+      isDone: false,
+    };
+    console.log(user);
+    console.log(user.habits, 'hello');
+    user.habits.push(newHabit); //pusheo al key followers
     await usersApi.updateOne(user.username, user);
-    res.json({ msg: "habito creado", data: newHabit });
+    res.json({ msg: 'habito creado', data: newHabit });
   } catch (error) {
     next({
       status: 400,
       errorContent: error,
-      message: "Algo salio mal",
+      message: 'Algo salio mal',
     });
   }
 };
@@ -94,48 +85,48 @@ const addHabit = async (req, res, next) => {
   try {
     let user = await usersApi.findOneById(req.user.id); //este user soy YO
     const habitDB = await habitsApi.findOneById(req.body.id);
-    const habitExist = user.habits.filter( habit => habitDB._id === habit._id);
-    if(!habitExist){
+    const habitExist = user.habits.filter((habit) => habitDB._id === habit._id);
+    if (!habitExist) {
       user.habits.push(habitDB);
       console.log(user);
       await usersApi.updateOne(user.username, user);
-      res.status(200).json({ msg: "hábito agregado", data: habitDB });
-    }else{
-      res.status(400).json({ msg: "El habito ya existe" });
+      res.status(200).json({ msg: 'hábito agregado', data: habitDB });
+    } else {
+      res.status(400).json({ msg: 'El habito ya existe' });
     }
   } catch (error) {
     next({
       status: 400,
       errorContent: error,
-      message: "Faltan datos",
+      message: 'Faltan datos',
     });
   }
 };
 
 const deActivateHabit = async (req, res, next) => {
   try {
-    const {id} = req.params;
-    habitsApi.deleteOne(id)
-    res.json("se modifico el archivo")
+    const { id } = req.params;
+    habitsApi.deleteOne(id);
+    res.json('se modifico el archivo');
   } catch (error) {
     next({
       status: 400,
       errorContent: error,
-      message: "Algo salio mal",
+      message: 'Algo salio mal',
     });
   }
 };
 
 const deleteHabit = async (req, res, next) => {
   try {
-    const {id} = req.params
-    await habitsApi.destroyOne(id)
-    res.json(`Habit with ID: ${id} was delete`)
+    const { id } = req.params;
+    await habitsApi.destroyOne(id);
+    res.json(`Habit with ID: ${id} was delete`);
   } catch (error) {
     next({
       status: 400,
       errorContent: error,
-      message: "Algo salio mal",
+      message: 'Algo salio mal',
     });
   }
 };
@@ -143,34 +134,33 @@ const deleteHabit = async (req, res, next) => {
 // controladores para solo obtener habitos del usuario
 const getMyHabits = async (req, res, next) => {
   try {
-    const id = req.user.id
-    const { habits } = await usersApi.findOneById(id)
-    res.json(habits)
+    const id = req.user.id;
+    const { habits } = await usersApi.findOneById(id);
+    res.json(habits);
   } catch (error) {
     next({
       status: 400,
       errorContent: error,
-      message: "Algo Salio mal",
+      message: 'Algo Salio mal',
     });
   }
 };
 
 // obtener mi habito por id
-const getMyHabitById = async(req, res, next) => {
+const getMyHabitById = async (req, res, next) => {
   try {
     const userId = req.user.id;
     const habitId = req.params.habitId;
 
-    const { habits } = await usersApi.findOneById(userId)
-    const habit = habits.filter(habit => habit.id === habitId)
-    
-    res.json(habit)
+    const { habits } = await usersApi.findOneById(userId);
+    const habit = habits.filter((habit) => habit.id === habitId);
 
+    res.json(habit);
   } catch (error) {
     next({
       status: 400,
       errorContent: error,
-      message: "Algo Salio mal",
+      message: 'Algo Salio mal',
     });
   }
 };
@@ -181,22 +171,25 @@ const updateIsDoneHabit = async (req, res, next) => {
     const userId = req.user.id;
     const habitId = req.params.habitId;
 
-    const { habits } = await usersApi.findOneById(userId);
-    const habit = habits.filter(habit => habit.id === habitId);
+    const { habits } = await usersApi.findOneByIdFollowers(userId);
+    const habit = habits.filter((habit) => habit.id === habitId);
 
-    if(habit[0].isDone){
-      res.json({message: 'Ya haz cumplido este habito por hoy'})
-    }else{
+    if (habit[0].isDone) {
+      res.json({ message: 'Ya haz cumplido este habito por hoy' });
+    } else {
       const newExperience = habit[0].experience + 10;
-      const result = await usersApi.updateIsDone(userId, habitId, newExperience );
-      res.json(result)
+      const result = await usersApi.updateIsDone(
+        userId,
+        habitId,
+        newExperience
+      );
+      res.json(result);
     }
-
   } catch (error) {
     next({
       status: 400,
       errorContent: error,
-      message: "Algo Salio mal",
+      message: 'Algo Salio mal',
     });
   }
 };
@@ -211,5 +204,5 @@ module.exports = {
   createCustomHabit,
   getMyHabits,
   getMyHabitById,
-  updateIsDoneHabit
-}
+  updateIsDoneHabit,
+};
